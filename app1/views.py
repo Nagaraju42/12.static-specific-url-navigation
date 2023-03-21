6e4c7d486a5a7d1bfd7149e99f6026867a6d450f
Nagaraju42 <nagarajukalngi@gmail.com>
2023-03-21 16:33:34 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Boyka(request):
+    return render(request,'Boyka.html')
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Ironman(request):
+    return render(request,'Ironman.html')
